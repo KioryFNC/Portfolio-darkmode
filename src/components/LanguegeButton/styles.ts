d6b447b49653633250c8f80b1762927e015f9505
KioryFNC <yuri.barros.luz.familia@gmail.com>
2025-07-07 17:40:46 -0300

    feat: styles LanguageButton

--- src/components/LanguegeButton/styles.ts
+++ src/components/LanguegeButton/styles.ts
@@ -1,7 +1,50 @@
 import styled from "styled-components";
 
-export const LanguageToggleWrapper = styled.label``;
-export const LanguageToggleInput = styled.input``;
-export const LanguageToggleSlider = styled.span``;
-export const PTLabel = styled.span``;
-export const ENLabel = styled.span``;
+export const LanguageToggleWrapper = styled.label`
+  margin-left: 1rem;
+  position: relative;
+  display: inline-block;
+  min-width: 4.5em;
+  height: 2.2em;
+  font-size: 1.7rem;
+  border-radius: 30px;
+  box-shadow: 0 0 10px ${(props) => props.theme.shadowColor};
+  cursor: pointer;
+
+  @media (max-width: 1280px) {
+    min-width: 4em;
+    max-width: 4em;
+  }
+`;
+
+export const LanguageToggleInput = styled.input`
+  width: 0;
+  height: 0;
+  opacity: 0;
+`;
+
+export const LanguageToggleSlider = styled.span`
+  position: absolute;
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  background-color: ${({ theme }) => theme.backgroundButton};
+  transition: background-color 0.4s ease-in-out;
+  border-radius: 30px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
+
+export const PTLabel = styled.span`
+  color: #fff;
+  font-size: 1rem;
+  font-weight: bold;
+`;
+
+export const ENLabel = styled.span`
+  color: #fff;
+  font-size: 1rem;
+  font-weight: bold;
+`;
