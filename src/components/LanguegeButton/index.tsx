a1868416beb58ad672c782e45ac3749cb5b80100
KioryFNC <yuri.barros.luz.familia@gmail.com>
2025-07-07 15:34:35 -0300

    feat: languageButton

--- src/components/LanguegeButton/index.tsx
+++ src/components/LanguegeButton/index.tsx
@@ -3,4 +3,21 @@ import * as Element from "./styles";
 
 export function LanguageButton() {
   const { language, toggleLanguage } = useLanguage();
+
+  return (
+    <Element.LanguageToggleWrapper>
+      <Element.LanguageToggleInput
+        type="checkbox"
+        checked={language === "en"}
+        onChange={toggleLanguage}
+      />
+      <Element.LanguageToggleSlider>
+        {language === "pt" ? (
+          <Element.PTLabel>PT</Element.PTLabel>
+        ) : (
+          <Element.ENLabel>EN</Element.ENLabel>
+        )}
+      </Element.LanguageToggleSlider>
+    </Element.LanguageToggleWrapper>
+  );
 }
--- src/components/LanguegeButton/styles.ts
+++ src/components/LanguegeButton/styles.ts
@@ -1,2 +1,7 @@
 import styled from "styled-components";
 
+export const LanguageToggleWrapper = styled.label``;
+export const LanguageToggleInput = styled.input``;
+export const LanguageToggleSlider = styled.span``;
+export const PTLabel = styled.span``;
+export const ENLabel = styled.span``;
