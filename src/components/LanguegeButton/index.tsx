d53cb01c86da893d519c7d9e3fb77541cea2b875
KioryFNC <yuri.barros.luz.familia@gmail.com>
2025-07-18 15:47:02 -0300

    sound button language & darkMode

--- src/components/LanguegeButton/index.tsx
+++ src/components/LanguegeButton/index.tsx
@@ -3,12 +3,21 @@ import * as Element from "./styles";
 
 import brFlag from "../../assets/Flags/Brasil.jpg";
 import usFlag from "../../assets/Flags/US.jpg";
+import ButtonClickSoundFile from "../../assets/sounds/buttonClick.mp3";
 
 export function LanguageButton() {
+  const ButtonClickSound = new Audio(ButtonClickSoundFile);
+  const playSound = (audio: HTMLAudioElement) => {
+    audio.currentTime = 0;
+    audio.play();
+  };
+
+  ButtonClickSound.volume = 0.1;
+
   const { language, toggleLanguage } = useLanguage();
 
   return (
-    <Element.LanguageToggleWrapper>
+    <Element.LanguageToggleWrapper onClick={() => playSound(ButtonClickSound)}>
       <Element.LanguageToggleInput
         type="checkbox"
         checked={language === "en"}
--- src/components/ThemeButton/index.tsx
+++ src/components/ThemeButton/index.tsx
@@ -1,12 +1,20 @@
 import { useTheme } from "../../contexts/ThemeContext";
 import * as Element from "./styles";
+import ButtonClickSoundFile from "../../assets/sounds/buttonClick.mp3";
 
 export function ThemeButton() {
+  const ButtonClickSound = new Audio(ButtonClickSoundFile);
+  const playSound = (audio: HTMLAudioElement) => {
+    audio.currentTime = 0;
+    audio.play();
+  };
+
+  ButtonClickSound.volume = 0.1;
 
   const { isDarkMode, toggleTheme } = useTheme();
 
   return (
-    <Element.ThemeToggleWrapper>
+    <Element.ThemeToggleWrapper onClick={() => playSound(ButtonClickSound)}>
       <Element.ThemeToggleInput
         type="checkbox"
         checked={isDarkMode}
